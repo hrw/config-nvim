e5673f88a16638d833838c58f9b36ce1a9c8c3d5
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2023-11-29 12:43:01 +0100

    initial configs

---
+++ init.lua
@@ -0,0 +1,56 @@
+
+vim.cmd [[ colorscheme peachpuff ]]
+
+-- From my .vimrc
+
+vim.opt.wrap = false        		-- don't wrap lines
+
+-- vim.opt.autochdir = true
+vim.opt.tabstop = 8     		-- a tab is 8 spaces
+vim.opt.backspace = indent,eol,start 	-- allow backspacing over everything in insert mode
+vim.opt.autoindent = true		-- always set autoindenting on
+vim.opt.copyindent = true		-- copy the previous indentation on autoindenting
+vim.opt.number = true  			-- always show line numbers
+vim.opt.shiftwidth = 8 			-- number of spaces to use for autoindenting
+vim.opt.shiftround = true		-- use multiple of shiftwidth when indenting with '<' and '>'
+vim.opt.showmatch = true    		-- set show matching parenthesis
+vim.opt.ignorecase = true   		-- ignore case when searching
+vim.opt.smartcase = true   		-- ignore case if search pattern is all lowercase,
+					--    case-sensitive otherwise
+vim.opt.smarttab = true     		-- insert tabs on the start of a line according to
+                  			--    shiftwidth, not tabstop
+vim.opt.hlsearch = true     		-- highlight search terms
+vim.opt.incsearch = true   		-- show search matches as you type
+
+vim.opt.history = 1000			-- remember more commands and search history
+vim.opt.undolevels = 1000		-- use many muchos levels of undo
+vim.opt.wildignore = "*.swp,*.bak,*.pyc,*.class"
+vim.opt.title = true			-- change the terminal's title
+vim.opt.visualbell = true		-- don't beep
+
+vim.opt.listchars = "tab:>·,trail:·,extends:>"
+
+-- syntax on
+-- syntax enable
+-- 
+-- autocmd FileType text setlocal textwidth=78
+vim.cmd [[ autocmd FileType python setlocal colorcolumn=80 ]]
+vim.cmd [[ autocmd FileType markdown setlocal textwidth=80 spell spelllang=en_gb ]]
+vim.cmd [[ autocmd FileType yaml setlocal cursorcolumn ]]
+vim.cmd [[ au BufRead,BufNewFile */playbooks/*.yml set filetype=yaml.ansible ]]
+-- 
+-- com Wq wq
+-- com W w
+-- 
+-- hi folded guibg=bg
+-- hi folded guifg=green
+-- 
+-- nmap <C-s> :w <Enter>
+-- map <C-s> <esc>:w<Enter>
+
+require 'plugins'
+
+local map = require("utils").map
+
+map("n", "<F2>", ":Telescope buffers<CR>")
+map("n", "<XF86Favorites>", ":Telescope keymaps<CR>")
---
+++ lua/plugins.lua
@@ -0,0 +1,67 @@
+-- Plugins will be downloaded under the specified directory.
+vim.call('plug#begin', '~/.config/nvim/plugged')
+local Plug = vim.fn['plug#']
+
+-- Declare the list of plugins.
+Plug('nvim-lua/plenary.nvim')
+Plug('nvim-telescope/telescope.nvim', {branch = '0.1.x'})
+Plug('nvim-treesitter/nvim-treesitter', {['do'] = vim.fn[':TSUpdate']})
+Plug('nvim-tree/nvim-web-devicons')
+
+Plug('folke/which-key.nvim')
+Plug('Valloric/MatchTagAlways', {ft = 'html'})
+Plug('tpope/vim-fugitive')
+
+-- LSP
+Plug('neovim/nvim-lspconfig')
+Plug('VonHeikemen/lsp-zero.nvim')
+Plug('williamboman/mason.nvim')
+Plug('williamboman/mason-lspconfig.nvim')
+
+-- Autocompletion
+Plug('hrsh7th/nvim-cmp')
+Plug('hrsh7th/cmp-buffer')
+Plug('hrsh7th/cmp-path')
+Plug('saadparwaiz1/cmp_luasnip')
+Plug('hrsh7th/cmp-nvim-lsp')
+Plug('hrsh7th/cmp-nvim-lua')
+
+-- Snippets
+Plug('L3MON4D3/LuaSnip')
+Plug('rafamadriz/friendly-snippets')
+
+Plug('powerman/vim-plugin-AnsiEsc')
+
+-- List ends here. Plugins become visible to Vim after this call.
+vim.call('plug#end')
+
+-- vim.opt.foldmethod = "expr"
+-- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldenable = false
+
+require'nvim-treesitter.configs'.setup {
+  indent = {
+    enable = true
+  }
+}
+
+require("mason").setup()
+require("mason-lspconfig").setup()
+
+local lsp = require('lsp-zero').preset({
+  name = 'minimal',
+  set_lsp_keymaps = true,
+  manage_nvim_cmp = true,
+  suggest_lsp_servers = false,
+})
+
+lsp.setup()
+
+vim.lsp.start({
+  name = 'my-server-name',
+  cmd = {'jedi-language-server'},
+  filetypes = {'python'},
+  root_dir = vim.fs.dirname(vim.fs.find({'setup.py', 'pyproject.toml'}, { upward = true })[1]),
+  single_file_support = true,
+})
+require'lspconfig'.jedi_language_server.setup{}

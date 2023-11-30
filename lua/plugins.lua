64cdebb34d20cc01c2bf2a87caf8ff04e2f96910
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2023-11-30 09:28:46 +0100

    download Plugin manager
    Instead of keeping it in repo let install newest version on first run.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,6 +1,17 @@
 -- Plugins will be downloaded under the specified directory.
-vim.call('plug#begin', '~/.config/nvim/plugged')
 local Plug = vim.fn['plug#']
+
+-- download Plug to have latest one if not present
+vim.cmd([[
+    if ! filereadable(system('echo -n "${XDG_CONFIG_HOME:-$HOME/.config}/nvim/autoload/plug.vim"'))
+        echo "Downloading junegunn/vim-plug to manage plugins..."
+        silent !mkdir -p ${XDG_CONFIG_HOME:-$HOME/.config}/nvim/autoload/
+        silent !curl "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" > ${XDG_CONFIG_HOME:-$HOME/.config}/nvim/autoload/plug.vim
+        autocmd VimEnter * PlugInstall
+    endif
+]])
+
+vim.call('plug#begin', '~/.config/nvim/plugged')
 
 -- Declare the list of plugins.
 Plug('nvim-lua/plenary.nvim')

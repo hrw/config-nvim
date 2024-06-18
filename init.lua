16c81504842122f6c57a9d4da907fb5b573c7058
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2024-06-18 12:49:09 +0200

    always show bufferline

--- init.lua
+++ init.lua
@@ -72,3 +72,5 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
+
+vim.opt.showtabline = 2
